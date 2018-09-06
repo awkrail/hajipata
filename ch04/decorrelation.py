25508b3552462b1da4cf6eb59e9fc72ce9c54b58
misogil0116 <lack_un@yahoo.co.jp>
2018-09-06 19:57:27 +0900

    fix

--- ch04/decorrelation.py
+++ ch04/decorrelation.py
@@ -5,44 +5,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+  # 必要なデータのロード
   setosa, versicolour, virginica = load_iris()
-  
-  setosa_N = len(setosa)
-  versicolour_N = len(versicolour)
-  virginica_N = len(virginica)
-
   all_data = np.array(setosa + versicolour + virginica, dtype=np.float32)
   N = all_data.shape[0]
 
+  # 平均と分散を計算する
   length, width = all_data[:, 0], all_data[:, 1]
   mu_l, mu_w = np.mean(length), np.mean(width)
   mu = np.array([mu_l, mu_w], dtype=np.float32)
   sigma_ll = (1/N) * np.sum((length - mu_l)**2)
   sigma_lw = (1/N) * np.sum((length - mu_l)*(width - mu_w))
   sigma_ww = (1/N) * np.sum((width - mu_w)**2)
-
   sigma = np.array([[sigma_ll, sigma_lw], [sigma_lw, sigma_ww]], dtype=np.float32)
-  result = np.linalg.eig(sigma)
-  for i in range(len(result[1])):
-    result[1][i] = result[1][i] / np.linalg.norm(result[1][i])
-  import ipdb; ipdb.set_trace()
-  lambdas, S_t = result[0], result[1].T
-  y = all_data @ S_t
 
-  # divide y with length, and width
+  # 固有値と固有ベクトルの計算
+  lambdas, S = np.linalg.eig(sigma)
+  y = np.dot(S.T, all_data.T).T
+
+  # x軸成分とy軸成分に分解
   n_length = y[:, 0]
   n_width = y[:, 1]
 
   # plot
-  setosa = np.concatenate((n_length[:setosa_N], n_width[:setosa_N])).reshape(setosa_N, 2)
-  versicolour = np.concatenate((n_length[setosa_N:setosa_N+versicolour_N], n_width[setosa_N:setosa_N+versicolour_N])).reshape(versicolour_N, 2)
-  virginica = np.concatenate((n_length[setosa_N+versicolour_N:], n_width[setosa_N+versicolour_N:])).reshape(virginica_N, 2)
-  
-  # plot
   plt.clf()
-  plt.scatter(setosa[:, 0], setosa[:, 1], marker='.', color='red')
-  plt.scatter(versicolour[:, 0], versicolour[:, 1], marker='o', color='blue')
-  plt.scatter(virginica[:, 0], virginica[:, 1], marker='^', color='green')
+  plt.scatter(n_length[:50], n_width[:50], marker='.', color='red')
+  plt.scatter(n_length[50:100], n_width[50:100], marker='o', color='blue')
+  plt.scatter(n_length[100:], n_width[100:], marker='^', color='green')
+  plt.ylim([-2.0, 3.0])
   plt.title('あやめデータ')
   plt.xlabel('花弁の長さ')
   plt.ylabel('花弁の幅')
--- ch04/normalization.py
+++ ch04/normalization.py
@@ -4,28 +4,13 @@ from common import load_iris, plot_scatter
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(setosa, versicolour, virginica, path):
-  # before_normalization
-  plt.clf()
-  plot_scatter(setosa, marker='.', color='red')
-  plot_scatter(versicolour, marker='o', color='blue')
-  plot_scatter(virginica, marker='^', color='green')
-  plt.title('あやめデータ')
-  plt.xlabel('花弁の長さ')
-  plt.ylabel('花弁の幅')
-  plt.savefig(path)
-
 if __name__ == "__main__":
+  # データのロード
   setosa, versicolour, virginica = load_iris()
-  plot(setosa, versicolour, virginica, "results/original.png")
-
-  setosa_N = len(setosa)
-  versicolour_N = len(versicolour)
-  virginica_N = len(virginica)
-
   all_data = np.array(setosa + versicolour + virginica, dtype=np.float32)
   N = all_data.shape[0]
 
+  # 平均と分散の計算
   length, width = all_data[:, 0], all_data[:, 1]
   mu_l, mu_w = np.mean(length), np.mean(width)
   mu = np.array([mu_l, mu_w], dtype=np.float32)
@@ -36,16 +21,12 @@ if __name__ == "__main__":
   # normalization
   n_length = (length - mu_l) / np.sqrt(sigma_ll)
   n_width = (width - mu_w) / np.sqrt(sigma_ww)
-
-  setosa = np.concatenate((n_length[:setosa_N], n_width[:setosa_N])).reshape(setosa_N, 2)
-  versicolour = np.concatenate((n_length[setosa_N:setosa_N+versicolour_N], n_width[setosa_N:setosa_N+versicolour_N])).reshape(versicolour_N, 2)
-  virginica = np.concatenate((n_length[setosa_N+versicolour_N:], n_width[setosa_N+versicolour_N:])).reshape(virginica_N, 2)
   
   # plot
   plt.clf()
-  plt.scatter(setosa[:, 0], setosa[:, 1], marker='.', color='red')
-  plt.scatter(versicolour[:, 0], versicolour[:, 1], marker='o', color='blue')
-  plt.scatter(virginica[:, 0], virginica[:, 1], marker='^', color='green')
+  plt.scatter(n_length[:50], n_width[:50], marker='.', color='red')
+  plt.scatter(n_length[50:100], n_width[50:100], marker='o', color='blue')
+  plt.scatter(n_length[100:], n_width[100:], marker='^', color='green')
   plt.title('あやめデータ')
   plt.xlabel('花弁の長さ')
   plt.ylabel('花弁の幅')
